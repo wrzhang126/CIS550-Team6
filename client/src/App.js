530d7f4dde8620aa30bda68cb2969267db1ddf9e
wrzhang126 <93270602+wrzhang126@users.noreply.github.com>
2021-11-23 19:07:26 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import SearchByArtistPage from './components/SearchByArtistPage';
+import SearchBySongPage from './components/SearchBySongPage';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
