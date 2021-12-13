32853c9d85413ab26cc41260a3bced34f7c1e2c8
davidbinstock <binstock.d@gmail.com>
2021-12-13 00:08:18 -0500

    deleted unused routes

--- Server/server.js
+++ Server/server.js
@@ -23,8 +23,6 @@ app.use(cors({ origin: "*" }));
 // ===========================================================================
 // ROUTING
 // ===========================================================================
-app.get("/", routes.hello);
-app.get("/artist", routes.test_db_query);
 app.get("/all/artists", routes.all_artists);
 app.get("/search/artists", routes.search_artists);
 app.get("/artist/:id", routes.get_artist_by_id);
@@ -32,7 +30,6 @@ app.get("/songs/artist/:id", routes.get_songs_by_artistid);
 app.get("/songs", routes.all_songs);
 app.get("/song", routes.get_song_by_id);
 app.get("/search/songs", routes.search_songs);
-
 app.get("/ranking", routes.awarded_artist);
 app.get("/awards", routes.get_awardstats_by_artist);
 app.get("/awards/billboard", routes.get_billboardsongs_by_artistid);
