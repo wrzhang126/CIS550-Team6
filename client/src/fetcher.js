c7a468de6a8ffdedc887566698cc2740a2e699d9
wrzhang126 <93270602+wrzhang126@users.noreply.github.com>
2021-12-12 21:02:39 -0500

    Update fetcher.js

--- client/src/fetcher.js
+++ client/src/fetcher.js
@@ -11,8 +11,38 @@ const getSong = async (id) => {
     })
     return res.json()
 }
-const getSongSearch = async (name, startYear, endYear, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/songs?name=${name}&startYear=${startYear}&endYear=${endYear}&page=${page}&pagesize=${pagesize}`, {
+const getSongSearch = async (name, danceabilityLow, danceabilityHigh, energyLow, energyHigh, livenessLow, livenessHigh, tempoLow, tempoHigh, valenceLow, valenceHigh, startYear, endYear, page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/songs?name=${name}&danceLow=${danceabilityLow}&danceHigh=${danceabilityHigh}&energyLow=${energyLow}&energyHigh=${energyHigh}&livenessLow=${livenessLow}&livenessHigh=${livenessHigh}&tempoLow=${tempoLow}&tempoHigh=${tempoHigh}&valenceLow=${valenceLow}&valenceHigh=${valenceHigh}&startYear=${startYear}&endYear=${endYear}&page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+const getAwardedArtists = async (page, pagesize) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/ranking?page=${page}&pagesize=${pagesize}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+const getAwardStat = async (artist_id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/awards?artist_id=${artist_id}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+const getBillboardSongs = async (artist_id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/awards/billboard?artist_id=${artist_id}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+const getGrammySongs = async (artist_id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/awards/grammy?artist_id=${artist_id}`, {
+        method: 'GET',
+    })
+    return res.json()
+}
+const getArtistById = async (artist_id) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/find/artist?id=${artist_id}`, {
         method: 'GET',
     })
     return res.json()
@@ -20,5 +50,10 @@ const getSongSearch = async (name, startYear, endYear, page, pagesize) => {
 export{
     getAllSongs,
     getSong,
-    getSongSearch
+    getSongSearch,
+    getAwardedArtists,
+    getAwardStat,
+    getBillboardSongs,
+    getGrammySongs,
+    getArtistById
 }
