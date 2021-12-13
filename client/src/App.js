b1c91f611cc13a9a0daf40a6b9f0fe6bfea77fec
wrzhang126 <93270602+wrzhang126@users.noreply.github.com>
2021-12-12 21:02:57 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import SearchByArtistPage from './components/SearchByArtistPage';
 import SearchBySongPage from './components/SearchBySongPage';
+import RankingPage from './components/RankingPage';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/" element= {<SearchByArtistPage/>}/>
           <Route path="/searchbyartist" element={<SearchByArtistPage/>}/>
           <Route path="/songs" element={<SearchBySongPage/>}/>
+          <Route path="/ranking" element={<RankingPage/>}/>
         </Routes>
       </Router>
     </div>
