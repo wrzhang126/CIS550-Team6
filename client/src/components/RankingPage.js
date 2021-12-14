50dd51bfc85ec8f5be799793838efb7c823908ba
Arnaud Mutabazi <61192534+Arnaud3@users.noreply.github.com>
2021-12-13 23:46:37 -0500

    Update RankingPage

--- client/src/components/RankingPage.js
+++ client/src/components/RankingPage.js
@@ -8,7 +8,7 @@ import {
   getArtistById,
   getArtistStats,
   getArtist,
-  getSpotifyRankedSongs
+  getSpotifyRankedSongs,
 } from "../fetcher";
 import React, { useState } from "react";
 import "./Filter.css";
@@ -73,7 +73,8 @@ export default function RankingPage() {
   const [grammySongs, setGrammySongs] = useState([]);
   const [spotifySongs, setSpotifySongs] = useState([]);
   const [selectedArtistId, setSelectedArtistId] = useState(
-    "0du5cEVh5yTK9QJze8zA0C");
+    "0du5cEVh5yTK9QJze8zA0C"
+  );
   const [awardStat, setAwardStat] = useState(null);
   const [artistInfo, setArtistInfo] = useState(null);
   const [activeTab, setActiveTab] = useState("tab1");
@@ -83,27 +84,27 @@ export default function RankingPage() {
     console.log(record);
 
     getAwardStat(record.artist_id).then((res) => {
-      console.log("award stats:")
+      console.log("award stats:");
       console.log(res.results);
       setAwardStat(res.results[0]);
     });
     getBillboardSongs(record.artist_id).then((res) => {
-      console.log("billboard songs:")
+      console.log("billboard songs:");
       console.log(res.results);
       setBillboardSongs(res.results);
     });
     getGrammySongs(record.artist_id).then((res) => {
-      console.log("grammy songs:")
+      console.log("grammy songs:");
       console.log(res.results);
       setGrammySongs(res.results);
     });
     getSpotifyRankedSongs(record.artist_id).then((res) => {
-      console.log("spotify ranked songs:")
+      console.log("spotify ranked songs:");
       console.log(res.results);
       setSpotifySongs(res.results);
     });
     getArtist(record.artist_id).then((res) => {
-      console.log("artist info:")
+      console.log("artist info:");
       console.log(res.results);
       setArtistInfo(res.results[0]);
     });
@@ -164,79 +165,101 @@ export default function RankingPage() {
   const contentList = {
     tab1: (
       <div>
-        <p> Number of Spotify Ranked Songs: {awardStat ? awardStat.num_songs_spotify : ""}</p>
-        <p> Number of Billboard Ranked Songs : {awardStat ? awardStat.num_songs_billboard : ""}</p>
-        <p> Number of Grammy Awards : {awardStat ? awardStat.num_songs_grammy : ""}</p>
+        <p>
+          {" "}
+          Number of Spotify Ranked Songs:{" "}
+          {awardStat ? awardStat.num_songs_spotify : ""}
+        </p>
+        <p>
+          {" "}
+          Number of Billboard Ranked Songs :{" "}
+          {awardStat ? awardStat.num_songs_billboard : ""}
+        </p>
+        <p>
+          {" "}
+          Number of Grammy Awards :{" "}
+          {awardStat ? awardStat.num_songs_grammy : ""}
+        </p>
       </div>
     ),
     tab2: (
-        // <div>
-        //     {spotifySongs.length > 0 ?
-        //         spotifySongs.map((song) => {
-        //             return(
-        //                 <div >   
-        //                     <p> Name : {song.title}</p>
-        //                     <p> Most Recent Time on Board : {song.latestweek}</p>
-        //                     <p> Times on Board : {song.num} </p>
-        //                 </div> 
-        //             )
-        //         })
-        //     : null}        
-        // </div>
+      // <div>
+      //     {spotifySongs.length > 0 ?
+      //         spotifySongs.map((song) => {
+      //             return(
+      //                 <div >
+      //                     <p> Name : {song.title}</p>
+      //                     <p> Most Recent Time on Board : {song.latestweek}</p>
+      //                     <p> Times on Board : {song.num} </p>
+      //                 </div>
+      //             )
+      //         })
+      //     : null}
+      // </div>
+      <div style={{ height: "200px", overflow: "scroll" }}>
         <List
-            size="small"
-            header={<div>Songs</div>}
-            // footer={<div>Footer</div>}
-            pagination
-            bordered
-            dataSource={spotifySongs}
-            renderItem={song=> <List.Item>
-                <div >   
-                    <p> Name : {song.title}</p>
-                    <p> Most Recent Time on Board : {song.latestweek}</p>
-                    <p> Times on Board : {song.num} </p>
-                </div> 
-            </List.Item>}
+          size="small"
+          header={<div>Songs</div>}
+          // footer={<div>Footer</div>}
+          pagination
+          bordered
+          dataSource={spotifySongs}
+          renderItem={(song) => (
+            <List.Item>
+              <div>
+                <p> Name : {song.title}</p>
+                <p> Most Recent Time on Board : {song.latestweek}</p>
+                <p> Times on Board : {song.num} </p>
+              </div>
+            </List.Item>
+          )}
         />
+      </div>
     ),
     tab3: (
-        <div>
-            {billboardSongs.length > 0 ?
-                billboardSongs.map((song) => {
-                    return(
-                        <div >   
-                            <a href={"https://open.spotify.com/track/"+song.song_id} target="_blank"> Track Title : {song.title}</a>
-                            <p> Most Recent Time on Board : {song.latestweek}</p>
-                            <p> Times on Board : {song.num} </p>
-                            <iframe 
-                                src={"https://open.spotify.com/embed/track/"+song.song_id} 
-                                width="300" 
-                                height="380" 
-                                frameborder="0" 
-                                allowtransparency="true" 
-                                allow="encrypted-media"></iframe>
-                        </div> 
-                    )
-                })
-            : null}        
-        </div>
+      <div>
+        {billboardSongs.length > 0
+          ? billboardSongs.map((song) => {
+              return (
+                <div>
+                  <a
+                    href={"https://open.spotify.com/track/" + song.song_id}
+                    target="_blank"
+                  >
+                    {" "}
+                    Track Title : {song.title}
+                  </a>
+                  <p> Most Recent Time on Board : {song.latestweek}</p>
+                  <p> Times on Board : {song.num} </p>
+                  <iframe
+                    src={"https://open.spotify.com/embed/track/" + song.song_id}
+                    width="300"
+                    height="380"
+                    frameborder="0"
+                    allowtransparency="true"
+                    allow="encrypted-media"
+                  ></iframe>
+                </div>
+              );
+            })
+          : null}
+      </div>
     ),
     tab4: (
-        <div>
-            {grammySongs.length > 0 ?
-                grammySongs.map((song) => {
-                    return(
-                        <div >   
-                            <p> Song Name : {song.title}</p>
-                            <p> Award : {song.award}</p>
-                            <p> Year Received : {song.year} </p>
-                        </div> 
-                    )
-                })
-            : null}        
-        </div>
+      <div>
+        {grammySongs.length > 0
+          ? grammySongs.map((song) => {
+              return (
+                <div>
+                  <p> Song Name : {song.title}</p>
+                  <p> Award : {song.award}</p>
+                  <p> Year Received : {song.year} </p>
+                </div>
+              );
+            })
+          : null}
+      </div>
     ),
-
   };
 
   return (
@@ -264,33 +287,33 @@ export default function RankingPage() {
         }}
       />
 
-      {artistInfo &&
-      <div style={{ width: "50%", margin: "auto" }}>
-        <Card
-          hoverable
-          size="small"
-          //   cover={
-          //     <img alt="example" src={artistInfo ? artistInfo.image_url : ""} />
-          //   }
-          title={artistInfo ? artistInfo.name : ""}
-          // extra={<a href="#">More</a>}
-          tabList={tabList}
-          activeTabKey={activeTab}
-          onTabChange={(key) => {
-            onTabChange(key);
-          }}
-        >
-          <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-            <img
-              style={{ width: "200px", height: "200px", objectFit: "cover" }}
-              alt="example"
-              src={artistInfo ? artistInfo.image_url : ""}
-            />
-            <div>{contentList[activeTab]}</div>
-          </div>
-        </Card>
+      {artistInfo && (
+        <div style={{ width: "50%", margin: "auto" }}>
+          <Card
+            hoverable
+            size="small"
+            //   cover={
+            //     <img alt="example" src={artistInfo ? artistInfo.image_url : ""} />
+            //   }
+            title={artistInfo ? artistInfo.name : ""}
+            // extra={<a href="#">More</a>}
+            tabList={tabList}
+            activeTabKey={activeTab}
+            onTabChange={(key) => {
+              onTabChange(key);
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+              <img
+                style={{ width: "200px", height: "200px", objectFit: "cover" }}
+                alt="example"
+                src={artistInfo ? artistInfo.image_url : ""}
+              />
+              <div>{contentList[activeTab]}</div>
+            </div>
+          </Card>
 
-        {/* <Tabs defaultActiveKey="1" centered >
+          {/* <Tabs defaultActiveKey="1" centered >
                     <TabPane tab="Tab 1" key="1">
                         <Card title="Card title" bordered={false} style={{ width: 300, margin: "auto", text-align:"center" }}>
                             <p>Card content</p>
@@ -306,7 +329,7 @@ export default function RankingPage() {
                     </TabPane>
                 </Tabs> */}
 
-        {/* <Carousel afterChange={onChange}>
+          {/* <Carousel afterChange={onChange}>
                     <div>
                         <h1 style={contentStyle}> {artistInfo ? artistInfo.name : ""}
                             <h3 style={{color : '#fff'}}> Grammy Award :  {awardStat ? awardStat.num_songs_grammy : 0}</h3>
@@ -344,8 +367,8 @@ export default function RankingPage() {
                         <h3 style={contentStyle}>4</h3>
                     </div>
                 </Carousel> */}
-      </div>
-      }
+        </div>
+      )}
     </div>
   );
 }
