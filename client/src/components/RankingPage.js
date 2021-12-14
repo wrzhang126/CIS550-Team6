22c46c47f22244675a61796e25c56eb34130f0d3
Arnaud Mutabazi <61192534+Arnaud3@users.noreply.github.com>
2021-12-13 20:20:46 -0500

    Update RankingPage.
    Added image to card

--- client/src/components/RankingPage.js
+++ client/src/components/RankingPage.js
@@ -1,16 +1,19 @@
-import Navigationbar from './Navbar';
-import Filter from './Filter';
-import { getAwardedArtists, getAwardStat, getBillboardSongs, getGrammySongs, getArtistById, getArtistStats, getArtist } from '../fetcher'
-import React, { useState } from "react";
-import './Filter.css';
+import Navigationbar from "./Navbar";
+import Filter from "./Filter";
 import {
-    Table,
-    Carousel,
-    Tabs,
-    Card
-} from 'antd'
-import { format } from 'd3-format';
-import { useEffect } from 'react';
+  getAwardedArtists,
+  getAwardStat,
+  getBillboardSongs,
+  getGrammySongs,
+  getArtistById,
+  getArtistStats,
+  getArtist,
+} from "../fetcher";
+import React, { useState } from "react";
+import "./Filter.css";
+import { Table, Carousel, Tabs, Card } from "antd";
+import { format } from "d3-format";
+import { useEffect } from "react";
 
 const { TabPane } = Tabs;
 
@@ -23,167 +26,182 @@ function onChange(a, b, c) {
 // };
 
 const contentStyle = {
-  height: '460px',
+  height: "460px",
   margin: "50px 250px 0 250px",
-  padding : "20px 50px 0 50px",
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'left',
-  background: '#364d79',
-  borderRadius: '10px',
-  boxShadow: "0 3px 6px -4px rgba(0, 0, 0, 0.12)"
+  padding: "20px 50px 0 50px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "left",
+  background: "#364d79",
+  borderRadius: "10px",
+  boxShadow: "0 3px 6px -4px rgba(0, 0, 0, 0.12)",
 };
 const artistColumns = [
+  {
+    title: "Artist Name",
+    dataIndex: "artist",
+    key: "artist",
+    sorter: (a, b) => a.name.localeCompare(b.name),
+  },
+  {
+    title: "Total Number of Songs",
+    dataIndex: "num_songs",
+    key: "num_songs",
+  },
+  {
+    title: "Number of Billboard Songs",
+    dataIndex: "num_songs_billboard",
+    key: "num_songs_billboard",
+  },
+  {
+    title: "Number of Spotify Ranked Songs",
+    dataIndex: "num_songs_spotify",
+    key: "num_songs_spotify",
+  },
+  {
+    title: "Number of Grammy Songs",
+    dataIndex: "num_songs_grammy",
+    key: "num_songs_grammy",
+  },
+];
+
+export default function RankingPage() {
+  const [artistsResults, setArtistsResults] = useState([]);
+  const [artistsStats, setArtistsStats] = useState([]);
+  const [billboardSongs, setBillboardSongs] = useState([]);
+  const [grammySongs, setGrammySongs] = useState([]);
+  const [selectedArtistId, setSelectedArtistId] = useState(
+    "0du5cEVh5yTK9QJze8zA0C"
+  );
+  const [awardStat, setAwardStat] = useState({});
+  const [artistInfo, setArtistInfo] = useState({});
+  const [activeTab, setActiveTab] = useState("tab1");
+
+  const handleRowSelection = (record) => {
+    console.log(record);
+
+    getAwardStat(record.artist_id).then((res) => {
+      console.log(res.results);
+      setAwardStat(res.results[0]);
+    });
+    getBillboardSongs(record.artist_id).then((res) => {
+      console.log(res.results);
+      setBillboardSongs(res.results);
+    });
+    getGrammySongs(record.artist_id).then((res) => {
+      console.log(res.results);
+      setGrammySongs(res.results);
+    });
+    getArtist(record.artist_id).then((res) => {
+      console.log(res.results);
+      setArtistInfo(res.results[0]);
+    });
+  };
+
+  const onTabChange = (key) => {
+    setActiveTab(key);
+  };
+
+  useEffect(() => {
+    getAwardedArtists().then((res) => {
+      console.log(res.results);
+      setArtistsResults(res.results);
+    });
+    getArtistStats().then((res) => {
+      console.log(res.results);
+      setArtistsStats(res.results);
+    });
+
+    getAwardStat(selectedArtistId).then((res) => {
+      console.log(res.results);
+      setAwardStat(res.results[0]);
+    });
+    getBillboardSongs(selectedArtistId).then((res) => {
+      console.log(res.results);
+      setBillboardSongs(res.results);
+    });
+    getGrammySongs(selectedArtistId).then((res) => {
+      console.log(res.results);
+      setGrammySongs(res.results);
+    });
+    getArtistById(selectedArtistId).then((res) => {
+      console.log(res.results);
+      setArtistInfo(res.results[0]);
+    });
+  }, []);
+
+  const tabList = [
     {
-      title: 'Artist Name',
-      dataIndex: 'artist',
-      key: 'artist',
-      sorter : (a, b) => a.name.localeCompare(b.name)
+      key: "tab1",
+      tab: "tab1",
     },
     {
-      title: 'Total Number of Songs',
-      dataIndex: 'num_songs',
-      key: 'num_songs'
+      key: "tab2",
+      tab: "tab2",
     },
-    {
-      title: 'Number of Billboard Songs',
-      dataIndex: 'num_songs_billboard',
-      key: 'num_songs_billboard'
-    },
-    {
-      title: 'Number of Spotify Ranked Songs',
-      dataIndex: 'num_songs_spotify',
-      key: 'num_songs_spotify'
-    },
-    {
-      title: 'Number of Grammy Songs',
-      dataIndex: 'num_songs_grammy',
-      key: 'num_songs_grammy'
-    }
-  ]
+  ];
 
+  const contentList = {
+    tab1: (
+      <div>
+        <p> Grammy Award : {awardStat ? awardStat.num_songs_grammy : 0}</p>
+        <p> Billboard : {awardStat ? awardStat.num_songs_billboard : 0}</p>
+        <p> Spotify : {awardStat ? awardStat.num_songs_spotify : 0}</p>
+      </div>
+    ),
 
-  export default function RankingPage() {
+    tab2: <p>content2</p>,
+  };
 
-    const [artistsResults, setArtistsResults] = useState([]);
-    const [artistsStats, setArtistsStats] = useState([]);
-    const [billboardSongs, setBillboardSongs] = useState([]);
-    const [grammySongs, setGrammySongs] = useState([]);
-    const [selectedArtistId, setSelectedArtistId] = useState("0du5cEVh5yTK9QJze8zA0C");
-    const [awardStat, setAwardStat] = useState({});
-    const [artistInfo, setArtistInfo] = useState({});
-    const [activeTab, setActiveTab] = useState('tab1');
+  return (
+    <div>
+      <div>
+        <Navigationbar />
+      </div>
+      <Table
+        id="filter-container"
+        style={{ width: "70vw", margin: "20px auto" }}
+        dataSource={artistsStats}
+        columns={artistColumns}
+        pagination={{
+          pageSizeOptions: [5, 10],
+          defaultPageSize: 5,
+          showQuickJumper: true,
+        }}
+        onRow={(record, rowIndex) => {
+          return {
+            onClick: (event) => {
+              handleRowSelection(record);
+            }, // click row
+          };
+        }}
+      />
+      <div style={{ width: "50%", margin: "auto" }}>
+        <Card
+          hoverable
+          size="small"
+          //   cover={
+          //     <img alt="example" src={artistInfo ? artistInfo.image_url : ""} />
+          //   }
+          title="Card title"
+          // extra={<a href="#">More</a>}
+          tabList={tabList}
+          activeTabKey={activeTab}
+          onTabChange={(key) => {
+            onTabChange(key);
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+            <img
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+              alt="example"
+              src={artistInfo ? artistInfo.image_url : ""}
+            />
+            <div>{contentList[activeTab]}</div>
+          </div>
+        </Card>
 
-    const handleRowSelection = (record) => {
-        console.log(record)
-
-        getAwardStat(record.artist_id).then(res => {
-            console.log(res.results)
-            setAwardStat(res.results[0])
-        })
-        getBillboardSongs(record.artist_id).then(res => {
-            console.log(res.results)
-            setBillboardSongs(res.results)
-        })
-        getGrammySongs(record.artist_id).then(res => {
-            console.log(res.results)
-            setGrammySongs(res.results)
-        })
-        getArtist(record.artist_id).then(res => {
-            console.log(res.results)
-            setArtistInfo(res.results[0])
-        })
-    };
-
-    const onTabChange = key => {
-        setActiveTab(key);
-    };
-
-    useEffect( () => {
-        getAwardedArtists().then(res => {
-            console.log(res.results)
-            setArtistsResults(res.results)
-        })
-        getArtistStats().then(res=>{
-            console.log(res.results)
-            setArtistsStats(res.results)
-        })
-
-        getAwardStat(selectedArtistId).then(res => {
-            console.log(res.results)
-            setAwardStat(res.results[0])
-        })
-        getBillboardSongs(selectedArtistId).then(res => {
-            console.log(res.results)
-            setBillboardSongs(res.results)
-        })
-        getGrammySongs(selectedArtistId).then(res => {
-            console.log(res.results)
-            setGrammySongs(res.results)
-        })
-        getArtistById(selectedArtistId).then(res => {
-            console.log(res.results)
-            setArtistInfo(res.results[0])
-        })
-    }, [])
-
-    const tabList = [
-        {
-          key: 'tab1',
-          tab: 'tab1',
-        },
-        {
-          key: 'tab2',
-          tab: 'tab2',
-        },
-      ];
-      
-      const contentList = {
-        tab1: <div>
-            <p> Grammy Award :  {awardStat ? awardStat.num_songs_grammy : 0}</p>
-            <p> Billboard :  {awardStat ? awardStat.num_songs_billboard : 0}</p>
-            <p> Spotify : {awardStat ? awardStat.num_songs_spotify: 0}</p>
-            </div>,
-        
-        tab2: <p>content2</p>,
-                            
-      };
-    
-
-    return (
-        <div>
-            <div><Navigationbar/></div>
-            <Table id="filter-container" 
-                style={{width: '70vw', margin: '20px auto'}} 
-                dataSource={artistsStats} 
-                columns={artistColumns} 
-                pagination={{ pageSizeOptions:[5, 10], defaultPageSize: 5, showQuickJumper:true }} 
-                onRow={(record, rowIndex) => {
-            return {
-              onClick: (event) => {
-                        handleRowSelection(record);
-                    }, // click row
-                    };
-                }}
-          />
-            <div style={{width:"90%", margin:"auto"}}>
-                
-                <Card
-                    style={{ width: '80%' }}
-                    hoverable
-                    // cover={<img alt="example" src={artistInfo ? artistInfo.image_url : ""} />}
-                    title="Card title"
-                    // extra={<a href="#">More</a>}
-                    tabList={tabList}
-                    activeTabKey={activeTab}
-                    onTabChange={key => {
-                        onTabChange(key);
-                    }}
-                >
-                    {contentList[activeTab]}
-                </Card>
-
-                {/* <Tabs defaultActiveKey="1" centered >
+        {/* <Tabs defaultActiveKey="1" centered >
                     <TabPane tab="Tab 1" key="1">
                         <Card title="Card title" bordered={false} style={{ width: 300, margin: "auto", text-align:"center" }}>
                             <p>Card content</p>
@@ -199,8 +217,7 @@ const artistColumns = [
                     </TabPane>
                 </Tabs> */}
 
-
-                {/* <Carousel afterChange={onChange}>
+        {/* <Carousel afterChange={onChange}>
                     <div>
                         <h1 style={contentStyle}> {artistInfo ? artistInfo.name : ""}
                             <h3 style={{color : '#fff'}}> Grammy Award :  {awardStat ? awardStat.num_songs_grammy : 0}</h3>
@@ -238,10 +255,7 @@ const artistColumns = [
                         <h3 style={contentStyle}>4</h3>
                     </div>
                 </Carousel> */}
-            </div>
-            
-        </div>
-        
-    );
-
+      </div>
+    </div>
+  );
 }
